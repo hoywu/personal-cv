042b53a6b6a257400af18c100ec2988b49189f75
hoywu <wu_git@outlook.com>
2024-04-07 22:25:47 +0800

    util: update TextUtil

--- packages/frontend/src/utils/TextUtil.ts
+++ packages/frontend/src/utils/TextUtil.ts
@@ -1,3 +1,10 @@
 export function removeWrap(text: string) {
   return text.replace(/\n\s+/g, '');
 }
+
+export function moreSpace(text: string | undefined, sep: string) {
+  const reg = new RegExp("\\S" + sep + "\\S");
+  if (text?.match(reg)) {
+    return text.replace(sep, ' ' + sep + ' ');
+  }
+}
